b0c0426b33653a18a3ce0ab8ffc49406d0d2fc4f
Prince Patel <patelprince314@gmail.com>
2026-01-19 00:14:05 +0530

    ad origin

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 // );
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:3000'], // Frontend URLs
+  origin: ['http://localhost:5173', 'https://gaonconnect.in', 'http://localhost:3000',], // Frontend URLs
   credentials: true,
   optionsSuccessStatus: 200,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
