32786f679fd850112b1407f111b0e7601ca4b905
princelawy <prince@lawyered.in>
2026-06-17 13:36:22 +0530

    email  code  changes

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,6 +15,9 @@ const sendOtpViaEmail = async (email, otp) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000,
   });
   await transporter.sendMail({
     from: `"GaonConnect" <${process.env.EMAIL_USER}>`,
